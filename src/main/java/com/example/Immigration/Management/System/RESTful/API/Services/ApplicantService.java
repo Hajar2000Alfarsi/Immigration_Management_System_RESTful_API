d2bd5ccc93e33d8ec4df2182d50f92c0b910dd9d
Hajar <hajar2000alfarsi@gmail.com>
2026-06-15 12:08:06 +0400

    save applicant

---
+++ src/main/java/com/example/Immigration/Management/System/RESTful/API/Services/ApplicantService.java
@@ -0,0 +1,40 @@
+package com.example.Immigration.Management.System.RESTful.API.Services;
+
+import com.example.Immigration.Management.System.RESTful.API.Entities.Applicant;
+import com.example.Immigration.Management.System.RESTful.API.Entities.Interview;
+import com.example.Immigration.Management.System.RESTful.API.Repositries.ApplicantRepository;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplicantService {
+    ApplicantRepository applicantRepository;
+
+    @Autowired
+    public ApplicantService(ApplicantRepository applicantRepository) {
+        this.applicantRepository = applicantRepository;
+    }
+
+    Interview interview;
+
+    @Autowired
+    public ApplicantService(Interview interview) {
+        this.interview = interview;
+    }
+
+    //save applicant
+    public Applicant saveApplicant(Applicant applicant) throws Exception{
+        if (applicant.getPassportNumber() == null || applicant.getPassportNumber().isEmpty()) {
+            throw new Exception("Invalid passport number");
+        }
+        if (applicant.getFirstName() == null || applicant.getLastName() == null) {
+            throw new Exception("First and Last name required");
+        }
+        if (applicant.getNationality() == null) {
+            throw new Exception("Nationality required");
+        }
+
+        return applicantRepository.save(applicant);
+    }
+}
