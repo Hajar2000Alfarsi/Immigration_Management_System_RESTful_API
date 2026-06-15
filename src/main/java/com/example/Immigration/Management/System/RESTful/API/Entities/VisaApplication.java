ff0b3f00939714137723fb0830a51533719362c9
Hajar <hajar2000alfarsi@gmail.com>
2026-06-15 10:14:56 +0400

    Create VisaApplication class

---
+++ src/main/java/com/example/Immigration/Management/System/RESTful/API/Entities/VisaApplication.java
@@ -0,0 +1,26 @@
+package com.example.Immigration.Management.System.RESTful.API.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class VisaApplication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Applicant applicant;
+
+    @ManyToOne
+    private ImmigrationOfficer handlingOfficer;
+
+    private String visaType;
+    private String status;
+    private String officerNotes;
+}
