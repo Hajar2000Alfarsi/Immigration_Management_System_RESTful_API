cab92df128e4655eb2d72ec39ee10be6ccf5c836
Hajar <hajar2000alfarsi@gmail.com>
2026-06-15 12:17:39 +0400

    overload save applicant

--- src/main/java/com/example/Immigration/Management/System/RESTful/API/Services/ApplicantService.java
+++ src/main/java/com/example/Immigration/Management/System/RESTful/API/Services/ApplicantService.java
@@ -35,6 +35,23 @@ public class ApplicantService {
             throw new Exception("Nationality required");
         }
 
+        applicant.setCriminalRecord(false);
         return applicantRepository.save(applicant);
     }
+
+    //Overloaded save applicant
+    public Applicant saveApplicant(String firstName, String lastName, String passportNumber, String
+            nationality) {
+        Applicant applicant = new Applicant();
+
+        applicant.setFirstName(firstName);
+        applicant.setLastName(lastName);
+        applicant.setPassportNumber(passportNumber);
+        applicant.setNationality(nationality);
+        applicant.setCriminalRecord(false);
+
+        return applicantRepository.save(applicant);
+    }
+
+
 }
